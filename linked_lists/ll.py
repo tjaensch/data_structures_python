6fbf84b5c489e7adcffff24d3f811cdc0ed4fac4
Thomas Jaensch <thomasjaensch@gmail.com>
2022-04-19 15:15:20 -0400

    Initial commit

---
+++ linked_lists/ll.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+
+ll = LinkedList(1)
+ll.append(2)
+
+print(ll.head.value)
+ll.print_list()
