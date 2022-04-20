4656eed4cead745eaa4da079c603a2f710539575
Thomas Jaensch <thomasjaensch@gmail.com>
2022-04-20 11:13:15 -0400

    Added stack

---
+++ stack/stack.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+    
+    def print_stack(self):
+        temp = self.top
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+
+    def pop(self):
+        if self.height == 0:
+            return None
+        else:
+            temp = self.top
+            self.top = temp.next
+            self.height -= 1
+            return temp
+
+
+stack = Stack(7)
+stack.push(23)
+stack.push(3)
+stack.push(11)
+stack.pop()
+stack.print_stack()
