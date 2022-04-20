f7e5bfc7d5c8ae194d632543cbfe4a11f7ebc5a4
Thomas Jaensch <thomasjaensch@gmail.com>
2022-04-20 11:38:05 -0400

    Added queue

---
+++ queue/queue.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        temp = self.first
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.first
+            self.first = temp.next
+            self.length -= 1
+            return temp
+
+
+queue = Queue(1)
+queue.enqueue(2)
+queue.print_queue()
+print(queue.dequeue())
+print(queue.dequeue())
+print(queue.dequeue())
