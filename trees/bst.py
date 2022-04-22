b00bde4555d9e1acbe34c55e9d988ec5fa72ac4e
Thomas Jaensch <thomasjaensch@gmail.com>
2022-04-22 11:34:53 -0400

    Added traversals

--- trees/bst.py
+++ trees/bst.py
@@ -61,6 +61,54 @@ class BinarySearchTree:
             current_node = current_node.right
         return current_node
 
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+
+    def dfs_pre_order(self):
+        results = []
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+
+    def dfs_post_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results
+
+    def dfs_in_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+
+
 def addBT(root):  
     if (root == None): 
         return 0
@@ -69,13 +117,18 @@ def addBT(root):
             
         
 bst = BinarySearchTree()
-bst.insert(2)
-bst.insert(1)
-bst.insert(10)
-bst.insert(3)
-bst.insert(4)
+bst.insert(47)
+bst.insert(21)
+bst.insert(76)
+bst.insert(18)
+bst.insert(27)
+bst.insert(52)
+bst.insert(82)
 
-print(addBT(bst.root))
+print(bst.BFS())
+print(bst.dfs_pre_order())
+print(bst.dfs_post_order())
+print(bst.dfs_in_order())
 
 
     
