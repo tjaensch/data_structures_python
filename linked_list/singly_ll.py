a89540826af0567fcb780b3b1e23c4cf8e8ab5f9
Thomas Jaensch <thomasjaensch@gmail.com>
2022-04-27 14:23:05 -0400

    Added singly ll implementation

---
+++ linked_list/singly_ll.py
@@ -0,0 +1,117 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+    
+    def insertSLL(self, value, location):
+            newNode = Node(value)
+            if self.head is None:
+                self.head = newNode
+                self.tail = newNode
+            else:
+                if location == 0:
+                    newNode.next = self.head
+                    self.head = newNode
+                elif location == -1:
+                    newNode.next = None
+                    self.tail.next = newNode
+                    self.tail = newNode
+                else:
+                    tempNode = self.head
+                    index = 0
+                    while index < location - 1:
+                        tempNode = tempNode.next
+                        index += 1
+                    nextNode = tempNode.next
+                    tempNode.next = newNode
+                    newNode.next = nextNode
+                    if tempNode == self.tail:
+                        self.tail=newNode
+    
+    def traverseSLL(self):
+        if self.head is None:
+            print("The Singly Linked List does not exist")
+        else:
+            node = self.head
+            while node is not None:
+                print(node.value)
+                node = node.next
+    
+    def searchSLL(self, nodeValue):
+        if self.head is None:
+           return "The list does not exist"
+        else:
+            node = self.head
+            while node is not None:
+                if node.value == nodeValue:
+                    return node.value
+                node = node.next
+            return "The value does not exist in this list"
+    
+    def deleteNode(self, location):
+        if self.head is None:
+            print("The SLL does not exist")
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+            elif location == -1:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    node = self.head
+                    while node is not None:
+                        if node.next == self.tail:
+                            break
+                        node = node.next
+                    node.next = None
+                    self.tail = node
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = nextNode.next
+    
+    def deleteEntireSLL(self):
+        if self.head is None:
+            print("The SLL does not exist")
+        else:
+            self.head = None
+            self.tail = None
+
+
+singlyLinkedList = SLinkedList()
+singlyLinkedList.insertSLL(1, 1)
+singlyLinkedList.insertSLL(2, 1)
+singlyLinkedList.insertSLL(3, 1)
+singlyLinkedList.insertSLL(4, 1)
+singlyLinkedList.insertSLL(5,-1)
+singlyLinkedList.insertSLL(0, 0)
+
+print([node.value for node in singlyLinkedList]) 
+
+print(singlyLinkedList.traverseSLL())
+
+
+
+
+
+
